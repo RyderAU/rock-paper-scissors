960c2032cdefa2bb5afd12188ee9a41fcb928bbe
Ryder <ryder.jacka@gmail.com>
2021-08-26 01:30:36 +1000

    Removed game function

--- script.js
+++ script.js
@@ -3,18 +3,10 @@ let computerSelection = '';
 let playerRounds = 0;
 let computerRounds = 0;
 
-// game();
-
 // search 'can you pass arguments into callback functions'
 
-const rockBtn = document.querySelector('#rockBtn');
-rockBtn.addEventListener('click', playRound(rockBtn.value));
-
-const paperBtn = document.querySelector('#paperBtn');
-paperBtn.addEventListener('click', playRound(paperBtn.value));
-
-const scissorsBtn = document.querySelector('#scissorsBtn');
-scissorsBtn.addEventListener('click', playRound(scissorsBtn.value));
+const buttons = document.querySelectorAll('#btn');
+buttons.forEach(button => button.addEventListener('click', playRound));
 
 const results = document.querySelector('#results');
 const score = document.querySelector('#score');
@@ -32,52 +24,34 @@ function computerPlay() {
     }
 }
 
-function playRound(selection) {
-    return function() {
-        playerSelection = selection;
-        computerSelection = computerPlay();
-        console.log('the player has selected ' + playerSelection)
-        console.log('the computer has selected ' + computerSelection);
+function playRound(e) {
+    playerSelection = e.target.value;
+    computerSelection = computerPlay();
+    console.log('the player has selected ' + playerSelection)
+    console.log('the computer has selected ' + computerSelection);
 
-        if (playerSelection === computerSelection) {
-            // draw
-            results.textContent = "it's a draw!";
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            // player wins
-            playerRounds++;
-            results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            // player wins
-            playerRounds++;
-            results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            // player wins
-            playerRounds++;
-            results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-        } else {
-            // computer wins
-            computerRounds++;
-            results.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
-        }
-        score.textContent = `player: ${playerRounds}, computer: ${computerRounds}`;
-        if (playerRounds === 5 || computerRounds === 5) {
-            results.textContent = 'GAME IS FINISHED';
-        }
-    }
-    
-}
-
-function game() {
-    while (playerRounds !== 5 && computerRounds !== 5) {
-        playRound(playerSelection, computerSelection);
-    }
-        
-    console.log(`SCORE: player - ${playerRounds} computer - ${computerRounds}`)
-    if (playerRounds === computerRounds) {
-        results.textContent = 'GAME IS A DRAW';
-    } else if (playerRounds > computerRounds) {
-        results.textContent = 'PLAYER WINS THE GAME';
+    if (playerSelection === computerSelection) {
+        // draw
+        results.textContent = "it's a draw!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        // player wins
+        playerRounds++;
+        results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        // player wins
+        playerRounds++;
+        results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        // player wins
+        playerRounds++;
+        results.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        results.textContent = 'COMPUTER WINS THE GAME';
+        // computer wins
+        computerRounds++;
+        results.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    score.textContent = `player: ${playerRounds}, computer: ${computerRounds}`;
+    if (playerRounds === 5 || computerRounds === 5) {
+        results.textContent = 'GAME IS FINISHED';
     }
 }
